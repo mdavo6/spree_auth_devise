e7bd74352ffeb5f7aebf71752d2725b96edcfc70
Michael Davidson <mdavo6@gmail.com>
2020-03-26 12:23:20 +1100

    Move customisation to store app

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,13 +1,7 @@
-# Merges users orders to their account after sign in and sign up.
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
     if user.is_a?(Spree::User)
-      orders_as_guest = Spree::Order.where(email: user.email, guest_token: auth.cookies.signed[:guest_token], user_id: nil)
-      if orders_as_guest.any?
-        most_recent_order = orders_as_guest.order('updated_at DESC').first
-        user.associate_address!(most_recent_order)
-      end
-      orders_as_guest.each do |order|
+      Spree::Order.where(email: user.email, guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
         order.associate_user!(user)
       end
     end
